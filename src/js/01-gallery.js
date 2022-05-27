05f3a91efa51454c00c3cd61eb52bc4bb0b0150d
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-27 22:02:46 +0300

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const createGaleryItemMarkup = (preview, original, description) =>
+  `<a class="gallery__item" style="display: contents;" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const galeryMarkup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc + createGaleryItemMarkup(preview, original, description),
+  ''
+);
+
+gallery.insertAdjacentHTML('beforeend', galeryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 // Change code below this line
 
 console.log(galleryItems);
