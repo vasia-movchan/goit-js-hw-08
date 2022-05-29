58b3a7fe28b48cdf5e94a24e3206fece02088226
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-29 17:35:27 +0300

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+} else {
+  localStorage.setItem('videoplayer-current-time', 0);
+}
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    console.log(seconds);
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
