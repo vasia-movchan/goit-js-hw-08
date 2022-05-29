83c124d82e7fa8da9fd49798dc846fe300124dc0
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-29 19:21:05 +0300

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+getDataOfLocalStorage();
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const feedback = {
+      email: email.value,
+      message: message.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(getDataOfLocalStorage());
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+});
+
+function getDataOfLocalStorage() {
+  const savedFeedback = localStorage.getItem('feedback-form-state');
+
+  try {
+    const parsedFeedback = JSON.parse(savedFeedback);
+    email.value = parsedFeedback.email;
+    message.value = parsedFeedback.message;
+  } catch (error) {
+    console.log('Locale Storage is empty');
+  }
+
+  return {
+    email: email.value,
+    message: message.value,
+  };
+}
